ff5a5d4b9cd42bd107e8d8ea462fca0fff2a4a4a
ThijssjihT <30879008+ThijssjihT@users.noreply.github.com>
2019-05-04 23:39:38 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+### Run file to setup after a fresh fedora install.
+### Last update 2019-05-04
+### Thijs Janssen
+
+### Setup smb mounts
+bash setupsmb.sh
+
+### Install rpmfusion repositories
+bash installrpmfusion.sh
+
+### Install printer drivers
+
+### Install additional software
