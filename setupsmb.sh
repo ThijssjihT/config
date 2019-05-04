e1df711489f1d743f50d70e267b5da1b61ecf11d
ThijssjihT <30879008+ThijssjihT@users.noreply.github.com>
2019-05-04 23:47:28 +0200

    Update setupsmb.sh

--- setupsmb.sh
+++ setupsmb.sh
@@ -1,1 +1,10 @@
-
+cp /etc/fstab ~/fstab
+cat <<EOT >> ~/fstab
+//NAS/Thijs /home/thijs/Documenten cifs credentials=/home/thijs/.smbcredentials,vers=2.0 0 0
+//NAS/music /home/thijs/Muziek cifs credentials=/home/thijs/.smbcredentials,vers=2.0 0 0
+//NAS/video /home/thijs/Video's cifs credentials=/home/thijs/.smbcredentials,vers=2.0 0 0
+//NAS/photo /home/thijs/Afbeeldingen cifs credentials=/home/thijs/.smbcredentials,vers=2.0 0 0
+//NAS/Download /home/thijs/Downloads/NASdownloads cifs credentials=/home/thijs/.smbcredentials,vers=2.0 0 0
+EOT
+sudo cp ~/fstab /etc/fstab
+sudo rm /etc/fstab
