c2c1891cc75bfe553d8198a2d3fe2e1449d64745
ThijssjihT <30879008+ThijssjihT@users.noreply.github.com>
2019-05-04 23:44:21 +0200

    Create installrpmfusion.sh

---
+++ installrpmfusion.sh
@@ -0,0 +1,4 @@
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf groupupdate core
+sudo dnf groupupdate multimedia
+sudo dnf groupupdate sound-and-video
