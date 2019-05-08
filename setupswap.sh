2879017bd50e934f5f43441304508d6eb7d01708
ThijssjihT <30879008+ThijssjihT@users.noreply.github.com>
2019-05-08 22:33:38 +0200

    Update setupswap.sh

--- setupswap.sh
+++ setupswap.sh
@@ -3,3 +3,13 @@ sudo chmod 600 /.swapfile
 sudo mkswap /.swapfile
 sudo swapon /.swapfile
 
+cp /etc/fstab ~/fstab
+cat <<EOT >> ~/fstab
+
+## added by Thijs's setup script
+## setupswap.sh
+
+/.swapfile none swap sw 0 0
+EOT
+sudo cp ~/fstab /etc/fstab
+sudo rm ~/fstab
