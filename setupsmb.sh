8fd96b8210d18c45ca1c0e614a39efee47584c27
ThijssjihT <30879008+ThijssjihT@users.noreply.github.com>
2019-05-08 22:27:59 +0200

    Update setupsmb.sh

--- setupsmb.sh
+++ setupsmb.sh
@@ -1,5 +1,7 @@
 cp /etc/fstab ~/fstab
 cat <<EOT >> ~/fstab
+
+## added by Thijs's setup script
 //NAS/Thijs /home/thijs/Documenten cifs credentials=/home/thijs/.smbcredentials,vers=2.0 0 0
 //NAS/music /home/thijs/Muziek cifs credentials=/home/thijs/.smbcredentials,vers=2.0 0 0
 //NAS/video /home/thijs/Video's cifs credentials=/home/thijs/.smbcredentials,vers=2.0 0 0
@@ -7,7 +9,7 @@ cat <<EOT >> ~/fstab
 //NAS/Download /home/thijs/Downloads/NASdownloads cifs credentials=/home/thijs/.smbcredentials,vers=2.0 0 0
 EOT
 sudo cp ~/fstab /etc/fstab
-sudo rm /etc/fstab
+sudo rm ~/fstab
 
 echo "Enter smb password for credentials file"
 
