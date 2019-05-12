09fa776c85aab8139083de60dbe71df8c678b093
ThijssjihT <30879008+ThijssjihT@users.noreply.github.com>
2019-05-12 13:28:10 +0200

    Update installrpmfusion.sh

--- installrpmfusion.sh
+++ installrpmfusion.sh
@@ -1,3 +1,7 @@
+### File used by setup.sh
+### Last update 2019-05-12
+### Thijs Janssen
+
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf groupupdate core
 sudo dnf groupupdate multimedia
